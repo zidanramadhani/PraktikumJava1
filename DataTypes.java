2b02a02dec5b5bb566c3ed7686c0affa0e3b3643
zidanoke <zidanramadhani007@gmail.com>
2021-05-30 00:27:04 +0800

    deklarasi variabel dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+ public static void main(String[]args){
+  String namaDepan = "Muhammad";
+  String namaTengah = "Zidan";
+  String namaBelakang = "Ramadhani";
+  int usia = 20;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89;
+  char nilaiAbjad = 'A';
+  boolean tampan = true;
+ 
+ }
+}
