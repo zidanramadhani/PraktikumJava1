60e7cfc182ba8f5a3cf35d560435426b0c6682e8
zidanoke <zidanramadhani007@gmail.com>
2021-06-12 00:07:28 +0800

    Input dengan Scanner

--- DataTypes.java
+++ DataTypes.java
@@ -1,7 +1,11 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class DataTypes{
  public static void main(String[]args){
+
+  Scanner scanner = new Scanner(System.in);  
+
   String namaDepan = "Muhammad";
   String namaTengah = "Zidan";
   String namaBelakang = "Ramadhani";
@@ -11,6 +15,19 @@ public class DataTypes{
   char nilaiAbjad = 'A';
   boolean tampan = true;
   
+  System.out.print("Input Nama Depan : ");
+  namaDepan = scanner.nextLine();
+  System.out.print("Input Usia : ");
+  usia = scanner.nextInt();
+  System.out.print("Input IPK : ");
+  ipk = scanner.nextDouble();
+  System.out.print("");
+  System.out.print("Input Nilai Abjad : ");
+  nilaiAbjad = scanner.next().charAt(0);
+  System.out.print("Tampan? : ");
+  tampan = scanner.nextBoolean();
+
+  System.out.println("============= OUTPUT =============");
   System.out.println("Nama Lengkap : " + " " 
 	+ namaDepan +" "
          + namaTengah +" "
